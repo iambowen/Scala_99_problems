d1a07dbda20e6c4bd097abe25576914f75ef5f17
Bowen Ma <iambowen.m@gmail.com>
2014-04-11 01:11:58 +0800

    add new solution for problem 1

--- src/p01.scala
+++ src/p01.scala
@@ -5,3 +5,8 @@ def last[A](list: List[A]) : A = {
 }
 
 
+def lastRecursive[A](list: List[A]) : A = list match {
+  case last :: Nil => last
+  case _ :: last => lastRecursive(last)
+  case _ => throw new NoSuchElementException
+}
