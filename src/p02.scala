cdf8f1f776b5cfbd3e6c42ab9c509d45ee3ef5e1
Bowen Ma <iambowen.m@gmail.com>
2014-05-23 00:35:13 +0800

    add recursive solution to p02

--- src/p02.scala
+++ src/p02.scala
@@ -4,14 +4,22 @@
 //     res0: Int = 5
 object p02 {
   def penultimate(list: List[A]): A = {
-    if (list.isEmpty) throw NoSuchElementException
+    if (list.isEmpty) throw new NoSuchElementException
     list.drop(2).head
   }
 
   def penultimateRecursive(list: List[A]): A = {
-    if (list.isEmpty) throw NoSuchElementException
-    if (list.size == 1) throw NoSuchElementException
+    if (list.isEmpty) throw new NoSuchElementException
+    if (list.size == 1) throw new NoSuchElementException
     if (list.size == 2) list.head
     else penultimateRecursive(list.tail)
   }
+
+
+   def penultimateRecursive[A](list: List[A]): A = list match {
+    case h :: _ :: Nil => h
+    case _ :: tail     => penultimateRecursive(tail)
+    case _             => throw new NoSuchElementException
+  }
+
 }
