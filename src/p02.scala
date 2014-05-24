9cef04df2e3eac8551e705c7f6d837c13235560d
Bowen Ma <iambowen.m@gmail.com>
2014-05-24 12:42:17 +0800

    update solution to problem 2

--- src/p02.scala
+++ src/p02.scala
@@ -3,12 +3,12 @@
 //     scala> penultimate(List(1, 1, 2, 3, 5, 8))
 //     res0: Int = 5
 object p02 {
-  def penultimate(list: List[A]): A = {
+  def penultimate[A](list: List[A]): A = {
     if (list.isEmpty) throw new NoSuchElementException
     list.drop(2).head
   }
 
-  def penultimateRecursive(list: List[A]): A = {
+  def penultimateRecursive[A](list: List[A]): A = {
     if (list.isEmpty) throw new NoSuchElementException
     if (list.size == 1) throw new NoSuchElementException
     if (list.size == 2) list.head
