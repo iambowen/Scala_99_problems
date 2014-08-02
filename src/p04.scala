b19b633cdb0d0ff81f5b5cf93997dbec8f3c45cc
Bowen Ma <iambowen.m@gmail.com>
2014-08-02 09:23:59 +0800

    update solution to question 4

--- src/p04.scala
+++ src/p04.scala
@@ -1,4 +1,9 @@
-def length(list: List[A]): Int = {
+def length[A](list: List[A]): Int = {
     if(list.isEmpty) 0
     else 1 + length(list.tail)
+}
+
+def length[A](list: List[A]): Int = list match {
+    case Nil => 0
+    case _ :: tail => 1 + length(tail)
 }
